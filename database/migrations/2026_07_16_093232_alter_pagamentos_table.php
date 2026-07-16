ca8840f285d1435828892622edf306b8003f98a5
Itiel Lima <itiel.lima.cavalcante@gmail.com>
2026-07-16 13:27:27 -0300

    Add associado snapshot fields to pagamentos table; include associado_nome and associado_cpf columns for better data tracking.

---
+++ database/migrations/2026_07_16_093232_alter_pagamentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('pagamentos', function (Blueprint $table) {
+
+            $table->dropForeign(['associado_id']);
+
+            $table->foreign('associado_id')->references('id')->on('associados')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('pagamentos', function (Blueprint $table) {
+
+            $table->dropForeign(['associado_id']);
+
+            $table->foreign('associado_id')->references('id')->on('associados')->cascadeOnDelete();
+        });
+    }
+};
